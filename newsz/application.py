c9973f7908430b062591032d5f63136762f7f27a
renantamashiro <tamashirorenan@gmail.com>
2020-08-04 00:37:25 -0300

    feat: add button class and style

--- newsz/application.py
+++ newsz/application.py
@@ -2,11 +2,12 @@ import toga
 from toga.style.pack import Pack, COLUMN, RIGHT
 
 from . import hackernews
+from newsz.elements import button
 
 
 class Newsz(toga.App):
     def startup(self):
-        self.main_window = toga.MainWindow(title=self.name, size=(1024,768))
+        self.main_window = toga.MainWindow(title=self.name, size=(1024, 768))
 
         # add content to the main window (ex: box, webview, etc)
 
@@ -14,13 +15,20 @@ class Newsz(toga.App):
         self.main_window.show()
 
 
-
-
 def build(app):
     # task: create a class for the right container
     right_container = toga.WebView(
         url='https://www.google.com/',
         style=Pack(flex=1))
+
+    content = toga.Label('Quando o sol se por\
+                         nada sobrará além da lua\
+                         então não desista fácil assim\
+                         do sol')
+
+    # scroll = toga.ScrollContainer(content=content)
+
+    right_container = toga.Box(children=[content])
 
     # task: create a class for the left container
     left_container = toga.Box(
@@ -35,7 +43,7 @@ def build(app):
     stories_data = {hackernews.Story(hackernews.API.story(i)) for i in stories}
 
     for story in stories_data:
-        btn = toga.Button(f'{story.title}', on_press=story.access_url)
+        btn = button.Article(f'{story.title}', action=story.access_url)
         left_container.add(btn)
 
     box = toga.Box(children=[left_container, right_container])
--- newsz/elements/button.py
+++ newsz/elements/button.py
@@ -1,4 +1,15 @@
 import toga
 
+from toga.style.pack import Pack
 
 
+class Article(toga.Button):
+    def __init__(self, title, action):
+        super().__init__(label=title, on_press=action)
+
+        self.label = title
+        self.on_press = action
+        self.style = Pack(
+            padding_top=40,
+            padding_bottom=40
+        )
