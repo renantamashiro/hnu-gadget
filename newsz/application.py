8923a440d3cf0409e2c5062650f635cd1ed3fcc7
renantamashiro <tamashirorenan@gmail.com>
2020-08-04 23:43:05 -0300

    feat: left container functions

--- newsz/application.py
+++ newsz/application.py
@@ -1,8 +1,8 @@
 import toga
-from toga.style.pack import Pack, COLUMN, RIGHT
+from toga.style.pack import Pack
 
 from . import hackernews
-from newsz.elements import button
+from newsz.containers import main_block
 
 
 class Newsz(toga.App):
@@ -16,36 +16,15 @@ class Newsz(toga.App):
 
 
 def build(app):
-    # task: create a class for the right container
     right_container = toga.WebView(
         url='https://www.google.com/',
         style=Pack(flex=1))
 
-    content = toga.Label('Quando o sol se por\
-                         nada sobrará além da lua\
-                         então não desista fácil assim\
-                         do sol')
-
-    # scroll = toga.ScrollContainer(content=content)
-
-    right_container = toga.Box(children=[content])
-
-    # task: create a class for the left container
-    left_container = toga.Box(
-        style=Pack(flex=1,
-                   direction=COLUMN,
-                   padding_top=50,
-                   alignment=RIGHT
-                   ))
-
-    # Right Container contructor
     stories = set(hackernews.API.stories(5))
     stories_data = {hackernews.Story(hackernews.API.story(i)) for i in stories}
 
-    for story in stories_data:
-        btn = button.Article(f'{story.title}', action=story.access_url)
-        left_container.add(btn)
-
+    left_container = main_block.LeftContainer()
+    left_container.generate(stories_data)
     box = toga.Box(children=[left_container, right_container])
 
     return box
--- newsz/elements/button.py
+++ newsz/elements/button.py
@@ -1,6 +1,14 @@
 import toga
 
-from toga.style.pack import Pack
+from toga.style.pack import Pack, CENTER
+
+
+class Label(toga.Box):
+    def __init__(self, article):
+        super().__init__(children=article)
+        self.style = Pack(
+            padding=6,
+        )
 
 
 class Article(toga.Button):
@@ -10,6 +18,18 @@ class Article(toga.Button):
         self.label = title
         self.on_press = action
         self.style = Pack(
-            padding_top=40,
-            padding_bottom=40
+            width=1024,
+            height=50
+        )
+
+
+class Auxiliary(toga.Button):
+    def __init__(self, title, action):
+        super().__init__(label=title, on_press=action)
+
+        self.label = title
+        self.on_press = action
+        self.style = Pack(
+            width=50,
+            height=50
         )
