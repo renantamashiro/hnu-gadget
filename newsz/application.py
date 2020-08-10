4f13aa2fa623ed3aed79c879cd6a3529b3250243
renantamashiro <tamashirorenan@gmail.com>
2020-08-09 22:57:17 -0300

    refactor: comment build func lines

--- newsz/application.py
+++ newsz/application.py
@@ -3,7 +3,6 @@ from toga.style.pack import Pack, COLUMN, RIGHT
 
 from . import hackernews
 from newsz.elements import button
-from newsz.containers import main_block
 
 
 class Newsz(toga.App):
@@ -56,32 +55,23 @@ class Newsz(toga.App):
             )
             self.button_box.add(button_field)
 
-    # def startup(self):
-        # pass
-        # self.main_window = toga.MainWindow(title=self.name, size=(1024, 768))
-
-        # add content to the main window (ex: box, webview, etc)
-
-        # show the main window
-        # self.main_window.show()
-
     def load_page(self, widget):
         self.webview.url = self.stories[widget.label]
 
+# def build(app):
+    # right_container = toga.WebView(
+        # url='https://www.google.com/',
+        # style=Pack(flex=1))
 
-def build(app):
-    right_container = toga.WebView(
-        url='https://www.google.com/',
-        style=Pack(flex=1))
+    # stories = set(hackernews.API.stories(10))
+    # stories_data = {
+    # hackernews.Story(hackernews.API.story(i)) for i in stories
+    # }
+    # left_container = main_block.LeftContainer()
+    # left_container.generate(stories_data)
+    # box = toga.Box(children=[left_container, right_container])
 
-    stories = set(hackernews.API.stories(10))
-    stories_data = {hackernews.Story(hackernews.API.story(i)) for i in stories}
-
-    left_container = main_block.LeftContainer()
-    left_container.generate(stories_data)
-    box = toga.Box(children=[left_container, right_container])
-
-    return box
+    # return box
 
 
 def main():
